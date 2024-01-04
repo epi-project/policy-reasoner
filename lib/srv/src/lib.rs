45da184222c0649f562462f8704edff03e7325ff
Lut99 <mullertim99@gmail.com>
2024-01-04 16:57:46 +0100

    Added graceful shutdown to the warp server in response to SIGINT or SIGTERM.

--- lib/srv/src/lib.rs
+++ lib/srv/src/lib.rs
@@ -1,20 +1,77 @@
 use std::convert::Infallible;
 use std::fmt::Debug;
 use std::sync::Arc;
+use std::time::Duration;
 
 use ::policy::{PolicyDataAccess, PolicyDataError};
 use audit_logger::AuditLogger;
 use auth_resolver::AuthResolver;
-use log::{debug, info};
+use error_trace::trace;
+use log::{debug, error, info, warn};
 use reasonerconn::ReasonerConnector;
 use serde::{Deserialize, Serialize};
 use state_resolver::StateResolver;
+use tokio::signal::unix::{signal, Signal, SignalKind};
 use warp::reject::Rejection;
 use warp::Filter;
 
 pub mod deliberation;
 pub mod models;
 pub mod policy;
+
+/// Function that returns a future that only returns if either SIGTERM or SIGINT has been sent to this process.
+///
+/// This is used to gracefully shut down the warp server, which takes an async function and will run until it returns. This mostly improves Docker-compatability, as it responds to `docker stop` and all that.
+///
+/// Only works on Unix.
+async fn graceful_signal() {
+    // Register a SIGTERM handler to be Docker-friendly
+    let term_handler: Option<Signal> = match signal(SignalKind::terminate()) {
+        Ok(handler) => Some(handler),
+        Err(err) => {
+            error!("{}", trace!(("Failed to register SIGTERM signal handler"), err));
+            warn!("Service will NOT shutdown gracefully on SIGTERM");
+            None
+        },
+    };
+
+    // Also register a SIGINT handler to be manual-friendly
+    let int_handler: Option<Signal> = match signal(SignalKind::interrupt()) {
+        Ok(handler) => Some(handler),
+        Err(err) => {
+            error!("{}", trace!(("Failed to register SIGINT signal handler"), err));
+            warn!("Service will NOT shutdown gracefully on SIGINT");
+            None
+        },
+    };
+
+    // Wait until we receive such a signal after which we terminate the server
+    match (term_handler, int_handler) {
+        (Some(mut term), Some(mut int)) => tokio::select! {
+            _ = term.recv() => {
+                info!("Received SIGTERM, shutting down gracefully...");
+            },
+
+            _ = int.recv() => {
+                info!("Received SIGINT, shutting down gracefully...");
+            },
+        },
+
+        (Some(mut term), None) => {
+            term.recv().await;
+            info!("Received SIGTERM, shutting down gracefully...");
+        },
+        (None, Some(mut int)) => {
+            int.recv().await;
+            info!("Received SIGINT, shutting down gracefully...");
+        },
+
+        // Just wait forever to not stop the warp server
+        (None, None) => loop {
+            tokio::time::sleep(Duration::from_secs(24 * 3600)).await;
+        },
+    }
+}
 
 pub struct Srv<L, C, P, S, PA, DA> {
     logger: L,
@@ -68,7 +125,8 @@ where
             }
         });
 
-        info!("Now serving at 127.0.0.1:3030; ready for requests");
-        warp::serve(index).run(([127, 0, 0, 1], 3030)).await;
+        let (addr, srv) = warp::serve(index).bind_with_graceful_shutdown(([127, 0, 0, 1], 3030), graceful_signal());
+        info!("Now serving at {addr}; ready for requests");
+        srv.await;
     }
 }
